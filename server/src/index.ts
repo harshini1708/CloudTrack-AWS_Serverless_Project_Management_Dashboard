6af4bbf95eff0fa23be47a53788cc7afaf41b857
harshini1708 <harshinipr17@gmail.com>
2024-06-25 10:10:00 -0700

    update: add index.ts

---
+++ server/src/index.ts
@@ -0,0 +1,72 @@
+import express, { Request, Response } from "express";
+import { PrismaClient } from "@prisma/client";
+// import express from "express";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+/* ROUTE IMPORTS */
+import projectRoutes from "./routes/projectRoutes";
+import taskRoutes from "./routes/taskRoutes";
+import searchRoutes from "./routes/searchRoutes";
+import userRoutes from "./routes/userRoutes";
+import teamRoutes from "./routes/teamRoutes";
+
+/* CONFIGURATIONS */
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+/* ROUTES */
+app.get("/", (req, res) => {
+  res.send("This is home route");
+});
+
+app.use("/projects", projectRoutes);
+app.use("/tasks", taskRoutes);
+app.use("/search", searchRoutes);
+app.use("/users", userRoutes);
+app.use("/teams", teamRoutes);
+
+const prisma = new PrismaClient();
+
+app.post("/create-user", async (req: Request, res: Response) => {
+      
+    try {
+      const { username, cognitoId, profilePictureUrl = "i1.jpg", teamId = 1 } = req.body;
+      const newUser = await prisma.user.create({
+        data: {
+                  username,
+                  cognitoId,
+                  profilePictureUrl,
+                  teamId,
+                },
+      });
+      res.json({message: "User created successfully", newUser });
+    } catch (error: any) {
+      res
+        .status(500)
+        .json({ message: `Error retrieving users: ${error.message}` });
+    }
+ });
+
+
+/* SERVER */
+// const port = (process.env.PORT) || 3000;
+// app.listen (port, () => {
+//   console.log(`Server running on part ${port}`);
+// });
+
+const port = Number(process.env.PORT) || 3000;
+app.listen(port, "0.0.0.0", () => {
+  console.log(`Server running on part ${port}`);
+});
+
+
